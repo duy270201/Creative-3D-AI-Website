3d4cc823ce178bf1514bee60894b3deff5566fe2
Duy <duy270201@gmail.com>
2023-06-12 21:04:22 +0700

    fix deployment 10

--- client/src/config/config.js
+++ client/src/config/config.js
@@ -1,6 +1,6 @@
 const config = {
   development: {
-    backendUrl: "https://creative-3d-ai-api.onrender.com/api/v1/dalle",
+    backendUrl: "https://creative-3d-ai-website-api.onrender.com/api/v1/dalle",
   },
   production: {
     backendUrl: "https://devswag.onrender.com/api/v1/dalle",
